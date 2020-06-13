cd85f5b73b660f075c3b2a2eee0b837f4633e872
scamp <scamp@lywxn.localdomain>
2020-06-13 12:24:39 +0100

    ended version

--- nonfilasc.py
+++ nonfilasc.py
@@ -15,7 +15,7 @@ def main():
             path = url.path
             print(f"in any case, need to see h as {hostname} & path as {path}")
             if hostname is not None:
-                # here just if there is None or not BUT not check for validating the returned variable is host has a correct value
+                # if there is None or not BUT not check for validating the returned variable is host has a correct value
                 print(f"{numb} - {line.strip()}: where hostname is {hostname} & path is {path}")
             else:
                 print(f"not valid url {line}")
