78b0a6c6f276af7dabd7c0d9b3cf3b06044f2430
scamp <scampovich@gmail.com>
2020-06-28 12:31:26 +0300

    add module structure

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 typical erros w/ python
 1. use open file with - only
 2. make chweck explicitly - i.e. is None
+3. try to create module
---
+++ funniest/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.001',
+      description='The funniest j in t wrld',
+      url='http://github.com/.../funniest',
+      author='Flying Circus',
+      author_email='typ@pyt.py',
+      license='MIT',
+      packages=['fun'],
+      zip_safe=False)
