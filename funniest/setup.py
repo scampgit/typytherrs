9f8c5c8b3330519922a3ca76350bab9c2b016f99
scamp <scampovich@gmail.com>
2020-06-28 12:57:09 +0300

    and again

--- funniest/setup.py
+++ funniest/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='funni',
       version='0.001',
-      description='The funniest j in t wrld',
+      description='The funnij in t wrld',
       url='http://github.com/.../funniest',
       author='Flying Circus',
       author_email='typ@pyt.py',
